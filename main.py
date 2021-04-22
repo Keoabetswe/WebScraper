d521867550e7b51a26e2613f52a657e4c627480a
Keoabetswe <keo.nthite@iress.com>
2021-04-22 11:49:36 +0200

    add BS lxml parser to extract data from element

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-imports requests 
+import requests 
 from bs4 import BeautifulSoup
 
 # request to the page to be scrapped
 mainContent = requests.get("https://takealot.com/")
-    print(mainContent.text)
+print(mainContent.text)
+
+soup = BeautifulSoup(mainContent.text, 'lxml')
+title = soup.find('h1', class_='cell auto').get_text()
+print(title)
+
+published = soup.find('small', class_='currency plus currency-module_currency_29IIm').get_text.strip()
+print(published)
