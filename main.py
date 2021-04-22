3abc51833b6cc1567fc4a60adfa1e57f116dd8c2
Keoabetswe <keo.nthite@iress.com>
2021-04-22 11:51:48 +0200

    add csv import and writer method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests 
 from bs4 import BeautifulSoup
+import csv
 
 # request to the page to be scrapped
 mainContent = requests.get("https://takealot.com/")
@@ -11,3 +12,8 @@ print(title)
 
 published = soup.find('small', class_='currency plus currency-module_currency_29IIm').get_text.strip()
 print(published)
+
+with open('takealotscraper.csv','w') as csvfile:
+    writer = csv.writer(csvfile)
+    for item in title_list:
+        writer.writerow([item])
