74876545c51d0adcc3bcdd37ef985244f1b7e263
Keoabetswe <keo.nthite@iress.com>
2021-04-23 21:44:22 +0200

    update takealot link to selected product

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ from bs4 import BeautifulSoup
 import csv
 
 # request to the page to be scrapped
-mainContent = requests.get("https://takealot.com/")
+mainContent = requests.get("https://www.takealot.com/rubiks-3x3-cube-new-version/PLID41679453/")
 print(mainContent.text)
 
 soup = BeautifulSoup(mainContent.text, 'lxml')
 title = soup.find('h1', class_='cell auto').get_text()
 print(title)
 
-published = soup.find('small', class_='currency plus currency-module_currency_29IIm').get_text.strip()
+published = soup.find('span', class_='currency plus currency-module_currency_29IIm').get_text.strip()
 print(published)
 
+# write to csv
 with open('takealotscraper.csv','w') as csvfile:
     writer = csv.writer(csvfile)
     for item in title_list:
