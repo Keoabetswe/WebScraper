78b63dc031e322c564c5977e234225150989e857
Keoabetswe <keo.nthite@iress.com>
2021-05-02 23:49:56 +0200

    add user agent headers

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from bs4 import BeautifulSoup
 import csv
 
 # request to the page to be scrapped
-mainContent = requests.get("https://www.takealot.com/rubiks-3x3-cube-new-version/PLID41679453/")
-print(mainContent.text)
+URL = "https://www.takealot.com/rubiks-3x3-cube-new-version/PLID41679453/"
+headers =  { 
+            "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
 
-soup = BeautifulSoup(mainContent.text, 'lxml')
-title = soup.find('h1', class_='cell auto').get_text()
-print(title)
+page = requests.get(URL, headers=headers)
+soup = BeautifulSoup(page.content, 'html.parser')
 
-published = soup.find('span', class_='currency plus currency-module_currency_29IIm').get_text.strip()
-print(published)
+title = soup.find(id="").get_text()
+price = soup.find(id="").get_text()
+
+print(price.strip())
 
 # write to csv
 with open('takealotscraper.csv','w') as csvfile:
     writer = csv.writer(csvfile)
-    for item in title_list:
+    for item in title:
         writer.writerow([item])
